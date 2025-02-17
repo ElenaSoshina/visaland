0bb6eb0c83cff81863d8e3c683771e99ebafc46e
Elena <elena@192.168.10.4>
2025-02-17 23:13:37 +0700

    changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,34 @@
-import React, {useState} from 'react';
-import styles from './Header.module.css';
+import React, { useState } from "react";
+import styles from "./Header.module.css";
+import Modal from "../Modal/Modal"; // Подключаем модальное окно
 
-function Header() {
-    const [menuOpen, setMenuOpen] = useState(false)
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false); // Состояние модалки
+
     return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <span className={styles.visa}>VISA</span>
-                <span className={styles.land}>LAND</span>
-            </div>
-            <button className={styles.burgerMenu} onClick={() => setMenuOpen(!menuOpen)}>
-                ☰
-            </button>
-            <nav>
-            <ul className={`${styles.navLinks} ${menuOpen ? styles.open : ''}`}>
-                    <li><a href="/">Главная</a></li>
-                    <li><a href="/about">О компании</a></li>
-                    <li><a href="/services">Услуги</a></li>
-                    <li><a href="/prices">Цены</a></li>
-                    <li><a href="/contacts">Контакты</a></li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header className={`${styles.header} ${isModalOpen ? styles.noFixed : ""}`}>
+                <div className={styles.logo}>
+                    <span className={styles.visa}>VISA</span>
+                    <span className={styles.land}>LAND</span>
+                </div>
+                <button className={styles.burgerMenu} onClick={() => setMenuOpen(!menuOpen)}>☰</button>
+                <nav>
+                    <ul className={`${styles.navLinks} ${menuOpen ? styles.open : ""}`}>
+                        <li><a href="/">Главная</a></li>
+                        <li><a href="/about">О компании</a></li>
+                        <li><a href="/services">Услуги</a></li>
+                        <li><a href="/prices">Цены</a></li>
+                        <li><a href="/contacts">Контакты</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            {/* Модальное окно */}
+            {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
+        </>
     );
-}
+};
 
 export default Header;
